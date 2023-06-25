b1459db383b738accc8b1b1cecf7d480eb19c98b
myducck <dmitro.kucherenko@gmail.com>
2023-06-25 13:47:41 +0300

    + response middleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,24 @@
 const responseMiddleware = (req, res, next) => {
-  // TODO: Implement middleware that returns result of the query
-  next();
+  try {
+    next();
+
+    if(res.err)
+      throw res.err;
+
+    if(!res.data || res.data.length === 0) {
+      res.status(404);
+      res.send("Data was not found");
+
+      return;
+    }
+
+    res.status(200);
+    res.send(res.data);
+  }
+  catch(e) {
+    res.status(400);
+    res.send(e);
+  }
 };
 
 export { responseMiddleware };
