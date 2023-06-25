19036393f30dca73753dbf04ac43a750c2c26d00
myducck <dmitro.kucherenko@gmail.com>
2023-06-25 13:49:55 +0300

    + user GET API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,41 @@
-import { Router } from "express";
+import { Router, json } from "express";
 import { userService } from "../services/userService.js";
 import {
   createUserValid,
   updateUserValid,
 } from "../middlewares/user.validation.middleware.js";
 import { responseMiddleware } from "../middlewares/response.middleware.js";
+import { userRepository } from "../repositories/userRepository.js";
 
 const router = Router();
 
-// TODO: Implement route controllers for user
+router.get(
+  "",
+  (req, res, next) => {
+    try {
+      res.data = userRepository.getAll();
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const data = userService.search({id: req.params.id});
+      res.data = data;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
 
 export { router };
--- services/userService.js
+++ services/userService.js
@@ -1,13 +1,16 @@
 import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
-  // TODO: Implement methods to work with user
-
   search(search) {
     const item = userRepository.getOne(search);
     if (!item) {
       return null;
     }
+    return item;
+  }
+
+  getAll() {
+    const item = userRepository.getAll();
     return item;
   }
 }
